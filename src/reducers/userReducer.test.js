391e35aed786d645a5332b7eadba8b532ad0436d
Justin Torrance <justin.torrance91@gmail.com>
2018-12-20 08:04:18 -0700

    Test userReducer

---
+++ src/reducers/userReducer.test.js
@@ -0,0 +1,29 @@
+import userReducer from './userReducer'
+import * as actions from '../actions'
+
+describe('userReducer', () => {
+  it('should return the default state', () => {
+    const expected = {}
+    const result = userReducer(undefined, {})
+    expect(result).toEqual(expected)
+  })
+  it('should return the state with a user', () => {
+    const action = {
+      type: 'SIGN_IN',
+      user: {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
+    }
+    const initialState = {}
+    const expected = {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
+    const result = userReducer(initialState, action)
+    expect(result).toEqual(expected)
+  })
+  it('should return the state with an empty object for user', () => {
+    const action = {
+      type: 'SIGN_OUT'
+    }
+    const initialState = {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
+    const expected = {}
+    const result = userReducer(initialState, action)
+    expect(result).toEqual(expected)
+  })
+})
