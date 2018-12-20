375f92d7043e65f38d93b6efd98d9f5f81995a94
Justin Torrance <justin.torrance91@gmail.com>
2018-12-20 11:05:12 -0700

    Fix conflicts on userReducer test

--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -1,10 +1,6 @@
 import userReducer from './userReducer'
 import * as actions from '../actions'
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 2efc3b4721b2e440fb0f020ac4bd2916bee39a15
 describe('userReducer', () => {
   it('should return the default state', () => {
     const expected = {}
@@ -12,7 +8,6 @@ describe('userReducer', () => {
     expect(result).toEqual(expected)
   })
   it('should return the state with a user', () => {
-<<<<<<< HEAD
     const user = { id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf' }
     const initialState = {}
     const expected = {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
@@ -23,24 +18,6 @@ describe('userReducer', () => {
     const initialState = {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
     const expected = {}
     const result = userReducer(initialState, actions.signOut())
-=======
-    const action = {
-      type: 'SIGN_IN',
-      user: {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
-    }
-    const initialState = {}
-    const expected = {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
-    const result = userReducer(initialState, action)
-    expect(result).toEqual(expected)
-  })
-  it('should return the state with an empty object for user', () => {
-    const action = {
-      type: 'SIGN_OUT'
-    }
-    const initialState = {id: 1, name: 'Justin', email: 'j@aol.com', password:'asdf'}
-    const expected = {}
-    const result = userReducer(initialState, action)
->>>>>>> 2efc3b4721b2e440fb0f020ac4bd2916bee39a15
     expect(result).toEqual(expected)
   })
 })
