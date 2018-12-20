1bfc1a7fe795a8c0b2bd01e65d9314e9ef3eb941
Justin Torrance <justin.torrance91@gmail.com>
2018-12-20 07:23:48 -0700

    Write tests for actions

---
+++ src/actions/actions.test.js
@@ -0,0 +1,29 @@
+import * as actions from '../actions'
+
+describe('actions', () => {
+  it('should return a type of LOAD_MOVIES with movies', () => {
+    const movies = []
+    const expected = {
+      type: 'LOAD_MOVIES',
+      movies
+    }
+    const result = actions.loadMovies(movies)
+    expect(result).toEqual(expected)
+  })
+  it('should return a type of SIGN_IN and a user', () => {
+    const user = {}
+    const expected = {
+      type: 'SIGN_IN',
+      user
+    }
+    const result = actions.signIn(user)
+    expect(result).toEqual(expected)
+  })
+  it('should return a type of SIGN_OUT', () => {
+    const expected = {
+      type: 'SIGN_OUT'
+    }
+    const result = actions.signOut()
+    expect(result).toEqual(expected)
+  })
+})
