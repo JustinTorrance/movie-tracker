e0bd964ef4f974f3a6df5afa8db5ffac08f32b24
Justin Torrance <justin.torrance91@gmail.com>
2018-12-27 11:17:39 -0700

    Test api calls

--- src/utils/apiCalls.test.js
+++ src/utils/apiCalls.test.js
@@ -81,4 +81,72 @@ describe('API', () => {
       expect(window.fetch).toHaveBeenCalledWith(expected1, expected2)
     })
   })
+
+  describe('addFavorite', () => {
+    it('calls fetch with the correct parameters when adding a new favorite', async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => {
+            return Promise.resolve()
+          },
+          ok: true
+        })
+      })
+      const movie = {title: 'title', poster_path: 'url', release_date: 2001, vote_average: 9.2, overview: 'overview'}
+      const movie_id = 1
+      const user_id = 2
+      const expected1 = 'http://localhost:3000/api/users/favorites/new'
+      const expected2 = {
+        method: 'POST',
+        body: JSON.stringify({movie_id, user_id, title: movie.title, poster_path: movie.poster_path, release_date: movie.release_date, vote_average: movie.vote_average, overview: movie.overview}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      await API.addFavorite(movie_id, user_id, movie)
+      expect(window.fetch).toHaveBeenCalledWith(expected1, expected2)
+    })
+  })
+
+  describe('getFavorites', () => {
+    it('calls fetch with the correct parameters', async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => {
+            return Promise.resolve()
+          },
+          ok: true
+        })
+      })
+      const mockUserId = 1
+      const url = `http://localhost:3000/api/users/${mockUserId}/favorites`
+      await API.getFavorites(mockUserId)
+      expect(window.fetch).toHaveBeenCalledWith(url)
+    })
+  })
+
+  describe('deleteFavorite', () => {
+    it('calls fetch with the correct parameters',  async () => {
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => {
+            return Promise.resolve()
+          },
+          ok: true
+        })
+      })
+      const user_id = 1
+      const movie_id = 2
+      const expected1 = `http://localhost:3000/api/users/${user_id}/favorites/${movie_id}`
+      const expected2 = {
+        method: 'DELETE',
+        body: JSON.stringify({user_id, movie_id}),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+      await API.deleteFavorite(user_id, movie_id)
+      expect(window.fetch).toHaveBeenCalledWith(expected1, expected2)
+    })
+  })
 })
